fabeb64307442de55170b4c0f4206cf5992a3582
zavislost <142222890+zavislost@users.noreply.github.com>
2025-02-17 16:13:44 +0100

    Update script.js

--- aka/script.js
+++ aka/script.js
@@ -5,38 +5,43 @@ document.getElementById('generate-button').addEventListener('click', function() 
     const outputImageDiv = document.getElementById('imageFullScreen');
     const container = document.getElementById('container');
 
-    var img = new Image();
-    img.src = 'img/akaTicket.png';
+    // Pokud je vybrán konkrétní tým, zobrazíme obrázek místo vykreslení na canvas
+    if (selectedTeam === "NějakýSpecifickýTým") {
+        // Zobrazit obrázek místo canvas
+        const img = new Image();
+        img.src = 'img/obrazekNamistoCanvas.png';  // Sem dejte cestu k obrázku, který chcete zobrazit
 
-    img.onload = function() {
-        let canvas = document.createElement("canvas");
-        let ctx = canvas.getContext("2d");
+        img.onload = function() {
+            outputImageDiv.src = img.src;
+            container.style.display = 'none';
+            outputImageDiv.style.display = 'block';
+        };
+    } else {
+        var img = new Image();
+        img.src = 'img/akaTicket.png';
 
-        canvas.width = img.naturalWidth;
-        canvas.height = img.naturalHeight;
+        img.onload = function() {
+            let canvas = document.createElement("canvas");
+            let ctx = canvas.getContext("2d");
 
-        ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
-        console.log("Načítání");
-        document.fonts.ready.then(() => {
-          console.log("načítání-font");
-          ctx.font = "normal 36px 'Open Sans'";  
-          ctx.fillStyle = "rgb(0, 0, 0)";
-          ctx.fillText(selectedTeam, 529, 368);
-          ctx.font = "normal 31px 'Open Sans'";
-          ctx.fillText(formatDate(selectedDate), 514, 416);
+            canvas.width = img.naturalWidth;
+            canvas.height = img.naturalHeight;
 
-          console.log("načítání-font2");
-          container.style.display = 'none';
-          outputImageDiv.src = canvas.toDataURL();
-          outputImageDiv.style.display = 'block';
-        }); 
-    };
+            ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+            ctx.font = "normal 36px 'Open Sans'";  
+            ctx.fillStyle = "rgb(0, 0, 0)";
+            ctx.fillText(selectedTeam, 529, 368);
+            ctx.font = "normal 31px 'Open Sans'";
+            ctx.fillText(formatDate(selectedDate), 514, 416);
 
+            container.style.display = 'none';
+            outputImageDiv.src = canvas.toDataURL();
+            outputImageDiv.style.display = 'block';
+        };
+    }
 });
 
 function formatDate(dateString) {
     const parts = dateString.split("-");
     return `${parts[2]}.${parts[1]}.${parts[0]}`;
 }
-
-console.log("JEDEM");
